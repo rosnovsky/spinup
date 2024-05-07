cf4987c0a86c23d20b11ea82e090ecfca1a08acb
Art Rosnovsky <art.rosnovsky@beyondid.com>
2024-05-07 11:03:39 -0700

    chore: tweaks

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
-import { prompt } from "enquirer";
 import { checkSystem } from "./commands/checkSystem";
-import { configureSystem } from "./commands/configureSystem";
 import {
   clearConsole,
   displaySystemInfo,
@@ -8,37 +6,14 @@ import {
   printBanner,
 } from "./utils/helpers";
 
-interface PromptResponse {
-  action: string;
-}
-
 async function mainMenu() {
   clearConsole();
   printBanner();
   await displaySystemInfo();
   const missingApps = checkSystem();
   await installApplications(missingApps);
-
-  // const response = await prompt<PromptResponse>({
-  //   type: "select",
-  //   name: "action",
-  //   message: "What would you like to do?",
-  //   choices: [
-  //     { name: "Configure system", value: "configure" },
-  //     { name: "Exit", value: "exit" },
-  //   ],
-  // });
-
-  // switch (response.action) {
-  //   case "Configure system":
-  //     break;
-  //   case "Exit":
-  //     console.log("Exiting...");
-  //     process.exit();
-  // }
-
-  // Optionally loop back to the main menu
-  // await mainMenu();
+  // TODO: Add font installation
+  // await installFonts();
 }
 
 mainMenu().catch((err) => {
