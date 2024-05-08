b5b0fc68d095de38e4127b6a7c3cbb046448c415
Art Rosnovsky <art.rosnovsky@beyondid.com>
2024-05-08 11:48:08 -0700

    chore: tweak visuals

--- rust/src/helpers.rs
+++ rust/src/helpers.rs
@@ -6,109 +6,90 @@
 use crate::config::Software;
 use colored::*;
 use figlet_rs::FIGfont;
-use futures::stream::{self, StreamExt};
 use prettytable::{format, Cell, Row, Table};
 use std::error::Error;
 use std::process::Command;
-use sysinfo::{Components, Cpu, System};
+use sysinfo::{Cpu, System, IS_SUPPORTED_SYSTEM};
 
-/// Checks whether applications are installed on the system.
-///
-/// ## Arguments
-///
-/// * `applications` - A vector of `Software` structs representing the applications to check.
-///
-/// ## Example
-///
-/// ```
-/// let applications = vec![
-///     Software {
-///         name: "make".to_string(),
-///         package: "make".to_string(),
-///         install: vec![InstallCommand {
-///             distro: "Fedora Linux".to_string(),
-///             command: "sudo dnf install -y make".to_string(),
-///         }],
-///         dependencies: None,
-///     },
-///     Software {
-///         name: "gcc".to_string(),
-///         package: "gcc".to_string(),
-///         install: vec![InstallCommand {
-///             distro: "Fedora Linux".to_string(),
-///             command: "sudo dnf install -y gcc".to_string(),
-///         }],
-///         dependencies: None,
-///     },
-/// ];
-/// let installed = check_applications(&applications).await?;
-/// ```
-///
-/// ## Returns
-/// This function returns a `Result` containing either the `Config` struct or an `Error` if the request fails.
-///
-/// ## Errors
-/// If the request fails, this function returns an `Error` with a descriptive message.
-pub async fn check_applications(
+pub async fn check_applications_status(
     applications: &[Software],
-) -> Result<Vec<Software>, Box<dyn Error>> {
+) -> Result<(Vec<Software>, Vec<Software>), Box<dyn Error>> {
     fn is_installed(app_name: &str) -> bool {
         let output = Command::new("which").arg(app_name).output();
-
         match output {
             Ok(output) => output.status.success(),
             Err(_) => false,
         }
     }
 
-    fn display_installation_status(installed: &[String], missing: &[String]) {
-        let mut table = Table::new();
-        table.set_format(*format::consts::FORMAT_NO_LINESEP);
-
-        table.set_titles(Row::new(vec![
-            Cell::new("        Installed        "),
-            Cell::new("           Missing          "),
-        ]));
-
-        let max_len = std::cmp::max(installed.len(), missing.len());
-        for i in 0..max_len {
-            let installed_cell = installed
-                .get(i)
-                .map_or(Cell::new(""), |name| Cell::new(name));
-            let missing_cell = missing.get(i).map_or(Cell::new(""), |name| Cell::new(name));
-
-            table.add_row(Row::new(vec![installed_cell, missing_cell]));
-        }
-        table.printstd();
-    }
-
     let mut installed = vec![];
     let mut missing = vec![];
 
     for app in applications {
-        let check = is_installed(&app.name);
-        if check {
-            installed.push(format!("{} {}", "✔".green(), app.name));
+        if is_installed(&app.name) {
+            installed.push(app.clone());
         } else {
-            missing.push(format!("{} {}", "✖".red(), app.name));
+            missing.push(app.clone());
         }
     }
 
+    Ok((installed, missing))
+}
+
+fn display_installation_status(installed: &Vec<Software>, missing: &Vec<Software>) {
+    let mut table = Table::new();
+    table.set_format(*format::consts::FORMAT_NO_LINESEP);
+
+    let installed_header = format!(
+        "{}{}{}",
+        "        ".bold(),
+        "Installed".underline().bold().green(),
+        "        "
+    );
+    let missing_header = format!(
+        "{}{}{}",
+        "        ".bold(),
+        "Missing".underline().bold().red(),
+        "        "
+    );
+
+    table.set_titles(Row::new(vec![
+        Cell::new(&installed_header),
+        Cell::new(&missing_header),
+    ]));
+
+    let max_len = std::cmp::max(installed.len(), missing.len());
+    for i in 0..max_len {
+        let installed_cell = installed
+            .get(i)
+            .map_or(Cell::new(""), |Software { name, .. }| {
+                Cell::new(format!("{} {}", "✔".green(), name).as_str())
+            });
+
+        let missing_cell = missing
+            .get(i)
+            .map_or(Cell::new(""), |Software { name, .. }| {
+                Cell::new(format!("{} {}", "✖".red(), name).as_str())
+            });
+        table.add_row(Row::new(vec![installed_cell, missing_cell]));
+    }
+    table.printstd();
+}
+
+pub async fn check_applications(applications: &[Software]) -> Result<(), Box<dyn Error>> {
+    let apps = check_applications_status(applications).await;
+    if apps.is_err() {
+        return Err(Box::new(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            "Failed to check applications",
+        )));
+    }
+
+    let (installed, missing) = apps.unwrap();
+
     display_installation_status(&installed, &missing);
 
-    let missing_apps = stream::iter(applications.iter())
-        .filter_map(|app| async move {
-            let check = is_installed(&app.name);
-            if !check {
-                Some(app.clone())
-            } else {
-                None
-            }
-        })
-        .collect::<Vec<_>>()
-        .await;
-
-    Ok(missing_apps)
+    Ok(())
 }
 
 /// Clears the console.
@@ -125,15 +106,21 @@ pub fn clear_console() {
     print!("\x1B[2J\x1B[3J\x1B[H");
 }
 
+// TODO: Should take in os name and cpu name as arguments and only be responsible for displaying the system info
 pub async fn display_system_info() {
+    if !IS_SUPPORTED_SYSTEM {
+        println!("This system is not supported.");
+        return;
+    }
+
     let mut system = System::new_all();
-    let cpu = system.cpus().iter().next().unwrap();
-
-    let frequency = Cpu::frequency(cpu);
-
     system.refresh_all();
 
-    let cpu_speed = frequency;
+    let cpu = system.cpus().iter().next().unwrap();
+
+    let brand = Cpu::brand(cpu);
+    let model = Cpu::name(cpu);
+
     let os_release = System::long_os_version().unwrap();
 
     let mut table = Table::new();
@@ -147,7 +134,11 @@ pub async fn display_system_info() {
 
     table.add_row(Row::new(vec![
         Cell::new(&format!("{}", "Processor".blue())),
-        Cell::new(&format!("{} MHz", cpu_speed.to_string().bright_green())),
+        Cell::new(&format!(
+            "{} {}",
+            brand.bright_green(),
+            model.bright_green()
+        )),
     ]));
 
     table.printstd(); // Prints the table to stdout
